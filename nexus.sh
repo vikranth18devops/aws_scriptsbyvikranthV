11e9b756b7173b6f20111a5e6a094d77689f5d2e
vikranthdevsecops <vikranth.devops18@gmail.com>
2022-11-24 20:01:40 +0530

    adding script files

---
+++ nexus.sh
@@ -0,0 +1,31 @@
+apt-get update -y
+
+echo "Install Java"
+apt-get install openjdk-8-jdk -y
+java -version
+
+echo "Install Nexus"
+useradd -M -d /opt/nexus -s /bin/bash -r nexus
+echo "nexus ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/nexus
+mkdir /opt/nexus
+wget https://sonatype-download.global.ssl.fastly.net/repository/downloads-prod-group/3/nexus-3.29.2-02-unix.tar.gz
+tar xzf nexus-3.29.2-02-unix.tar.gz -C /opt/nexus --strip-components=1
+chown -R nexus:nexus /opt/nexus
+
+vi /opt/nexus/bin/nexus.vmoptions
+
+replace with ../ to ./ ----after replacing
+
+vi /opt/nexus/bin/nexus.rc ---file will open add the "nexus" user
+
+After adding nexus user
+
+sudo -u nexus /opt/nexus/bin/nexus start
+
+nano /opt/nexus/bin/nexus.vmoptions
+
+
+
+```
+https://www.howtoforge.com/how-to-install-and-configure-nexus-repository-manager-on-ubuntu-20-04/
+```
